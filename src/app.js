12369f80c93838a0d6e63301aa41f0f4f51c033d
Tark <tark_zeng@163.com>
2018-04-14 17:51:49 +0800

    用react脚手架创建项目

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-/**
- * Created by QD_zengta on 2018/3/15.
- */
-import React from 'react';
-import ReactDOM from 'react-dom';
-class App extends React.Component {
-    render() {
-        return (
-            <div></div>
-        );
-    }
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
 }
+
+export default App;
